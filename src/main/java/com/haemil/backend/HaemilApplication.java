e6690b522a4308d77112c7d99a27911cfbcdfb40
yubin <garmalhy1@gmail.com>
2023-05-23 22:04:23 +0900

    [FEAT] init

---
+++ src/main/java/com/haemil/backend/HaemilApplication.java
@@ -0,0 +1,13 @@
+package com.haemil.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HaemilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HaemilApplication.class, args);
+	}
+
+}
