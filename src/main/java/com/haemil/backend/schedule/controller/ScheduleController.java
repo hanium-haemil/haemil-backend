0220dec76cff91ef3875a73791eac31edf36259a
SemiLee <smvb2001@naver.com>
2023-08-26 12:16:14 +0900

    [CHORE] add schedule api

--- src/main/java/com/haemil/backend/schedule/controller/ScheduleController.java
+++ src/main/java/com/haemil/backend/schedule/controller/ScheduleController.java
@@ -1,34 +1,34 @@
 package com.haemil.backend.schedule.controller;
 
-import com.fasterxml.jackson.databind.ser.Serializers;
 import com.haemil.backend.global.config.BaseResponse;
-import com.haemil.backend.global.config.ResponseStatus;
 import com.haemil.backend.global.exception.BaseException;
+import com.haemil.backend.map.service.MapService;
 import com.haemil.backend.schedule.dto.ScheduleRequestDto;
 import com.haemil.backend.schedule.dto.ScheduleResponseDto;
 import com.haemil.backend.schedule.entity.Schedule;
-import com.haemil.backend.schedule.repository.ScheduleRepository;
 import com.haemil.backend.schedule.service.ScheduleService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-
-import static com.haemil.backend.global.config.ResponseStatus.BAD_REQUEST;
-import static com.haemil.backend.global.config.ResponseStatus.INTERNAL_SERVER_ERROR;
+import java.time.LocalDate;
+import java.util.List;
 
 
 @RestController
 @RequestMapping("/schedules")
+@Slf4j
 public class ScheduleController {
 
     public final ScheduleService scheduleService;
+    public final MapService mapService;
 
     @Autowired
-    public ScheduleController(ScheduleService scheduleService) {
+    public ScheduleController(ScheduleService scheduleService, MapService mapService) {
         this.scheduleService = scheduleService;
+        this.mapService = mapService;
     }
 
     //일정 추가 API
@@ -37,24 +37,33 @@ public class ScheduleController {
 
         try {
             ScheduleResponseDto createSchedule = scheduleService.createSchedule(scheduleRequestDto);
+
+            // 맵 API 호출하여 맵 URL 얻어오는 부분
+            String mapUrl = mapService.getMapUrl(scheduleRequestDto.getPlace());
+            // 응답에 맵 URL을 포함하여 리턴
+            createSchedule.setMapUrl(mapUrl);
+
             BaseResponse<ScheduleResponseDto> response = new BaseResponse<>(createSchedule);
             return response.convert();
 
         } catch (BaseException e) {
             return new BaseResponse<>(e.getStatus()).convert();
+        } catch (com.haemil.backend.global.config.BaseException e) {
+            throw new RuntimeException(e);
         }
 
     }
 
     //주어진 날짜에 해당하는 일정 조회 API
     @GetMapping("/getSchedule")
-    public ResponseEntity<BaseResponse> getSchedulesByDate(@RequestParam("localDate") @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate localDate){
-       try {
-           List<Schedule> schedules = scheduleService.getSchedule(localDate);
-           return new BaseResponse<>(schedules).convert();
-       }catch (BaseException e) {
-           return new BaseResponse<>(e.getStatus()).convert();
-       }
+    public ResponseEntity<BaseResponse> getSchedulesByDate(@RequestParam("localDate") @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate localDate) {
+        try {
+            List<Schedule> schedules = scheduleService.getSchedule(localDate);
+            return new BaseResponse<>(schedules).convert();
+
+        } catch (BaseException e) {
+            return new BaseResponse<>(e.getStatus()).convert();
+        }
     }
 
 
@@ -65,7 +74,7 @@ public class ScheduleController {
             List<Schedule> todaySchedules = scheduleService.getTodaySchedules();
             return new BaseResponse<>(todaySchedules).convert();
 
-        } catch(BaseException e) {
+        } catch (BaseException e) {
             return new BaseResponse<>(e.getStatus()).convert();
         }
     }
@@ -73,7 +82,7 @@ public class ScheduleController {
 
     //일정 삭제 API
     @DeleteMapping("/schedule/{scheduleId}")
-    public ResponseEntity<BaseResponse> deleteSchedule(@PathVariable Long scheduleId){
+    public ResponseEntity<BaseResponse> deleteSchedule(@PathVariable Long scheduleId) {
         try {
             Long deletedId = scheduleService.deleteSchedule(scheduleId);
             BaseResponse<Long> response = new BaseResponse<>(deletedId);
@@ -86,15 +95,26 @@ public class ScheduleController {
 
     //일정 수정 API
     @PatchMapping("/schedule/{scheduleId}")
-    public ResponseEntity<Schedule> updateSchedule(@PathVariable Long scheduleId, @RequestBody ScheduleRequestDto requestDto) {
-        Schedule updatedSchedule = scheduleService.updateSchedule(scheduleId, requestDto);
+    public ResponseEntity<BaseResponse> updateSchedule(@PathVariable Long scheduleId, @RequestBody ScheduleRequestDto requestDto) {
+        try {
+            Schedule updatedSchedule = scheduleService.updateSchedule(scheduleId, requestDto);
 
-        if (updatedSchedule != null) {
-            return ResponseEntity.ok(updatedSchedule);
-        } else {
-            return ResponseEntity.notFound().build();
+            log.debug("requestDto.getPlace() = {}",requestDto.getPlace());
+            // 맵 API 호출하여 맵 URL 얻어오는 부분
+            String mapUrl = mapService.getMapUrl(requestDto.getPlace());
+            System.out.println(mapUrl);
+            // 응답에 맵 URL을 포함하여 리턴
+            updatedSchedule.setMapUrl(mapUrl);
+
+            ScheduleResponseDto responseDto = new ScheduleResponseDto(updatedSchedule);
+            BaseResponse<ScheduleResponseDto> response = new BaseResponse<>(responseDto);
+            return response.convert();
+
+        } catch (BaseException e) {
+            return new BaseResponse<>(e.getStatus()).convert();
+
+        } catch (com.haemil.backend.global.config.BaseException e) {
+            throw new RuntimeException(e);
         }
     }
-
-
 }
---
+++ src/main/java/com/haemil/backend/schedule/dto/ScheduleResponseDto.java
@@ -0,0 +1,61 @@
+package com.haemil.backend.schedule.dto;
+
+import com.haemil.backend.schedule.entity.RepeatType;
+import com.haemil.backend.schedule.entity.Schedule;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+//schedule에서 값을 가져올 때 직접적인 entity 대신 앞에 써줌
+//클라이언트에게 응답할 때 필요한 속성들 추가
+@NoArgsConstructor
+@Getter
+@Setter
+public class ScheduleResponseDto {
+
+    private String mapUrl;
+
+    private Long id;
+
+    private LocalDate localDate;
+
+    private DayOfWeek dayOfWeek;
+
+    private LocalTime time;
+
+    private String content;
+
+    private String place;
+
+    private Boolean done;
+
+    private String medicine;
+
+    private RepeatType repeatType;
+
+
+    public ScheduleResponseDto(Schedule schedule){
+        this.id = schedule.getId();
+
+        this.localDate = schedule.getLocalDate();
+
+        this.dayOfWeek = schedule.getDayOfWeek();
+
+        this.time = schedule.getTime();
+
+        this.content = schedule.getContent();
+
+        this.place = schedule.getPlace();
+
+        this.done = schedule.getDone();
+
+        this.medicine = schedule.getMedicine();
+
+        this.repeatType = schedule.getRepeatType();
+
+    }
+}
